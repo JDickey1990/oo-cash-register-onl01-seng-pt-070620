5ee896a82010e15456586e147121dbd6f15936db
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-08-02 19:27:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+class CashRegister 
+ 
+attr_accessor :total, :discount
 
+def initialize(discount = 20) 
+  @total= 0
+  @discount = discount
+end 
+ 
+def add_item(title, price, quantity = 1)
+  @total+= price * quantity
+end
+
+def apply_discount
+ percentage = (1.0 - (0.01 * discount))
+ self.total = @total * percentage 
+ "After the discount, the total comes to $#{self.total.to_i}."
+
+end
+
+
+
+end
